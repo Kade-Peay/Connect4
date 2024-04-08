ba335de0d1d3639bae90495688c7d23dff70003b
Kade Peay <kadepeay@gmail.com>
2024-04-08 15:47:21 -0600

    blank game window display correctly

--- connect.py
+++ connect.py
@@ -1,9 +1,22 @@
-#!/usr/bin/env Python3
-import pygame
+# use pygame 
+# connect 4 game. blue vs. yellow. 7 X 6 grid
+# first implement player vs. player, then player vs computer 
 
-pygame.display.init()
-win = pygame.display.set_mode((800,700))
+import pygame 
 
-running = True
-while running:
-    win
+# initialize pygame and get window showing 
+pygame.init()
+back = (192,192,192)
+gameWindow = pygame.display.set_mode((800,700))
+pygame.display.set_caption('Connect 4')
+gameWindow.fill(back)
+clock = pygame.time.Clock()
+running = True 
+while running: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.update()
+    clock.tick(60)
+pygame.quit()
+quit()
